c886f5d6746d3cde9601b3f3840d4a7cf7788dc0
pbraunstein <pbraunstein12@gmail.com>
2015-04-18 20:22:54 -0400

    First commit

---
+++ dl.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+from sys import argv
+from sys import exit
+import csv
+import random
+
+# CONSTANTS
+DB_FILE = "/Users/Phippe/Developer/Self_Betterment/dl.csv"
+
+def main():
+    if len(argv) > 1:
+        addEntry()
+        exit(0)
+
+    data = buildList()
+
+    displayAndTest(random.choice(data))
+    
+    exit(0)
+
+# Reads in DB file into list of lists 
+def buildList():
+    toReturn = []
+    with open(DB_FILE, 'r') as csvfile:
+        csvReader = csv.reader(csvfile)
+        for row in csvReader:
+            toReturn.append([x.strip() for x in row])
+
+    return toReturn
+
+# Show German word, waits for input, shows English defs, wait for inputs
+# shows Partizip II returns
+# structure of input as below
+# [german, partzip, english1;english2]
+def displayAndTest(toTest):
+    raw_input("Was bedeutet " + toTest[0] + "?")
+    for defin in toTest[2].split(";"):
+        print defin
+    print
+    raw_input("Was ist das Partzip und Hilfsverb?")
+    print toTest[1]
+    print
+
+
+# Adds an entry to the current entry in the db
+def addEntry():
+    entryValid()  # if returns, entry okay
+
+    toAdd = []
+
+    currentData = buildList()
+
+    # create new entry
+    defs = argv[4:]
+    toAdd.append(argv[2])
+    toAdd.append(argv[3])
+    toAdd.append(";".join(defs))
+
+    currentData.append(toAdd)
+
+    writeOut(currentData)
+
+
+# Enforces these invariants:
+# 1. First command line arg is exactly -a or -Add
+# 2. there are at least 4 args on command line (not including script name)
+# Exits with error message if any invariant violated
+def entryValid():
+    invalid = False
+    if len(argv) < 5:
+        print "ERROR: Not Enough Arguments Provided"
+        print argv
+        invalid = True
+    elif argv[1] != "-a" and argv[1] != "-Add":
+        print "ERROR: Must supply -a or -Add as First Argument"
+        invalid = True
+
+    if invalid:
+        print "USAGE:", argv[0], "-a", "Deutsches_Verb",
+        print "Partizip_mit_Hilfsverb", 
+        print "englische_Definitionen (beliebige Zahl)"
+
+        exit(1)
+
+
+# Overwrites DB_FILE with all the entries in currentData
+def writeOut(currentData):
+    with open(DB_FILE, 'w') as csvFile:
+        csvWriter = csv.writer(csvFile)
+
+        for row in currentData:
+            csvWriter.writerow(row)
+
+        print ",".join(currentData[-1])
+
+
+if __name__ == '__main__':
+    main()
---
+++ getByFamily.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+# Looks in the DB file for words maching the ending supplied
+# to the script. Looks only in the first column of the CSV.
+
+DB_FILE = "/Users/Phippe/Developer/Self_Betterment"
+
+from sys import exit
+from sys import argv
+import re
+import random
+
+from dl import buildList
+
+def main():
+    option = None  # Default
+    if len(argv) == 1:  # No options
+        print "ERROR: Not enough command line parameters"
+        usage()
+    elif len(argv) == 2:
+        root = argv[1]
+    elif len(argv) == 3:
+        option = argv[1]
+        root = argv[2]
+    else:
+        print "ERROR: Too many command line parameters"
+        usage()
+
+    verbTable = buildList()
+
+    filteredTable = filterVerbTable(verbTable, root)
+
+    if len(filteredTable) == 0:
+        print "No verbs match root:", root
+        exit(1)
+
+    if not option:
+        option = "-t"  # Default behavior
+
+    if option == "-t":
+        test(filteredTable)
+        view(filteredTable)
+    elif option == "-s":
+        view(filteredTable)
+    elif option == "-h":
+        usage()
+    else:
+        print "ERROR: Invalid option:", option
+        usage()
+
+    exit(0)
+
+
+# Prints usage and exits non-zero
+def usage():
+    print "USAGE:", argv[0], "-h", "root", "--", "view this message"
+    print "USAGE:", argv[0], "-s", "root", "--", "just print"
+    print "USAGE:", argv[0], "-t", "root", "--", "test then print"
+    print "Default option is -t"
+    exit(1)
+
+# Returns a table only with those entries whose first elements
+# match .*root$
+def filterVerbTable(verbTable, root):
+    regEx = re.compile(".*" + root)
+
+    return filter(lambda x: regEx.match(x[0]), verbTable)
+
+# Asks user meaning of all words selected, waiting for enter key input
+# to show the answer. Then shows the Hilfsverb + Partizipzwei without being
+# prompted. Some basic formatting
+def test(data):
+    random.shuffle(data)
+
+    def iTest(entry):
+        raw_input(entry[0] + ":")
+        for defn in entry[2].split(";"):
+            print "    " + defn
+        print "    " + entry[1]
+        print
+
+    map(iTest, data)
+
+
+
+# Right now sorts umlauts to the end. Might fix this later
+def view(data):
+    data.sort(key = lambda x: x[0])
+    def printEl(x):
+        print x[0] + " --- " + x[2]
+
+    map(printEl, data)
+
+
+if __name__ == "__main__":
+    main()
